8b22b01d40b00808c672167a40f136466abe6cb4
Dev <dev786.1@gmail.com>
2022-06-02 19:34:12 +0530

    Array and Array List practice

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age=0;
+        try (Scanner sc = new Scanner(System.in)) {
+            do {
+               try {
+                   System.out.println("Please enter your age : ");
+                   age = sc.nextInt();
+                   if (age > 18)
+                       System.out.println("you are adult");
+                   else
+                       System.out.println("You are under 18");
+               }catch(Exception e){
+
+                   break;
+
+               }finally {
+                   sc.close();
+               }
+
+            } while (age > 1);
+        }
     }
 }

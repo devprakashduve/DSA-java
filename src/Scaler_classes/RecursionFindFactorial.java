a0ba372586a831992c8d17f377b676923c2a56ee
Dev <dev786.1@gmail.com>
2022-07-10 11:05:56 +0530

    recursion practice find factorial

---
+++ src/Scaler_classes/RecursionFindFactorial.java
@@ -0,0 +1,15 @@
+package Scaler_classes;
+
+public class RecursionFindFactorial {
+    public static void main(String[] args) {
+        int A=5;
+        int res=solve(A);
+        System.out.println(res);
+    }
+    static int solve(int A) {
+        if(A==1){
+            return 1;
+        }
+        return solve(A-1)*A;
+    }
+}

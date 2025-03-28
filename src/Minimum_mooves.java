60ecd4bcd1599a22d535c6fc1b4e82bbe66c61bd
devprakashduve <dev876.1@gmail.com>
2025-03-28 21:13:34 +0530

    minimum moovs implementation

---
+++ src/Minimum_mooves.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int minMoves(int[] nums) {
+        int min = nums[0];
+        long sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        return (int) (sum - (long) min * nums.length);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Test cases
+        int[] test1 = {1, 2, 3};
+        System.out.println(solution.minMoves(test1)); // Output: 3
+
+        int[] test2 = {1, 1, 1};
+        System.out.println(solution.minMoves(test2)); // Output: 0
+
+        int[] test3 = {5, 2, 1};
+        System.out.println(solution.minMoves(test3)); // Output: 5
+    }
+}
